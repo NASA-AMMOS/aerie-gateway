681f8dd2abbff5489367ba265ddf143c7969398d
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2021-10-13 15:38:24 -0700

    refactor: set env on start

--- src/packages/playground/playground.ts
+++ src/packages/playground/playground.ts
@@ -2,7 +2,7 @@ import type { Express } from 'express';
 import expressPlayground from 'graphql-playground-middleware-express';
 
 export default (app: Express) => {
-  const { GQL_PLAYGROUND_ENDPOINT = 'http://localhost:27184/' } = process.env;
+  const { GQL_PLAYGROUND_ENDPOINT = '' } = process.env;
   const initPlayground = (expressPlayground as any).default;
   app.get('/playground', initPlayground({ endpoint: GQL_PLAYGROUND_ENDPOINT }));
 };
