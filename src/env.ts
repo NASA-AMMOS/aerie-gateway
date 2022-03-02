ea31b3c69b14c49e24ab751afbacf902b3a89722
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2022-03-02 09:49:19 -0800

    Added Command Server Environment variables

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,8 @@
 export type Env = {
   AUTH_TYPE: string;
   AUTH_URL: string;
+  COMMANDING_SERVER_PORT: string;
+  COMMANDING_SERVER_URL: string;
   GQL_API_URL: string;
   PORT: string;
   POSTGRES_AERIE_MERLIN_DB: string;
@@ -18,6 +20,8 @@ export type Env = {
 export const defaultEnv: Env = {
   AUTH_TYPE: 'cam',
   AUTH_URL: 'https://atb-ocio-12b.jpl.nasa.gov:8443/cam-api',
+  COMMANDING_SERVER_PORT: '3000',
+  COMMANDING_SERVER_URL: 'http://localhost',
   GQL_API_URL: 'http://localhost:8080/v1/graphql',
   PORT: '9000',
   POSTGRES_AERIE_MERLIN_DB: 'aerie_merlin',
@@ -51,6 +55,10 @@ export function getEnv(): Env {
 
   const AUTH_TYPE = env['AUTH_TYPE'] ?? defaultEnv.AUTH_TYPE;
   const AUTH_URL = env['AUTH_URL'] ?? defaultEnv.AUTH_URL;
+  const COMMANDING_SERVER_PORT =
+    env['COMMANDING_SERVER_PORT'] ?? defaultEnv.COMMANDING_SERVER_PORT;
+  const COMMANDING_SERVER_URL =
+    env['COMMANDING_SERVER_URL'] ?? defaultEnv.COMMANDING_SERVER_URL;
   const GQL_API_URL = env['GQL_API_URL'] ?? defaultEnv.GQL_API_URL;
   const PORT = env['PORT'] ?? defaultEnv.PORT;
   const POSTGRES_AERIE_MERLIN_DB =
@@ -79,6 +87,8 @@ export function getEnv(): Env {
   return {
     AUTH_TYPE,
     AUTH_URL,
+    COMMANDING_SERVER_PORT,
+    COMMANDING_SERVER_URL,
     GQL_API_URL,
     PORT,
     POSTGRES_AERIE_MERLIN_DB,
