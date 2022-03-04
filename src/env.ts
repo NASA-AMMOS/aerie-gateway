ed0edab4b4892c24fa9b68bcfebe65804d26c366
camargo <camargo.cac@gmail.com>
2022-03-04 13:27:39 -0800

    release: version 0.10.1

--- src/env.ts
+++ src/env.ts
@@ -29,7 +29,7 @@ export const defaultEnv: Env = {
   RATE_LIMITER_FILES_MAX: 1000,
   RATE_LIMITER_LOGIN_MAX: 1000,
   RATE_LIMITER_UI_VIEWS_MAX: 1000,
-  VERSION: '0.10.0',
+  VERSION: '0.10.1',
 };
 
 /**
