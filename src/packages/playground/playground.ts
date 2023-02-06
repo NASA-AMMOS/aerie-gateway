5a8fdf8da33ddc6e9c21b888ee4e86220b7908e7
Chris <camargo.cac@gmail.com>
2023-02-06 15:31:58 -0800

    Remove playground dependency and routes (#18)

--- src/packages/playground/playground.ts
+++
@@ -1,9 +0,0 @@
-import type { Express } from 'express';
-import expressPlayground from 'graphql-playground-middleware-express';
-import { getEnv } from '../../env.js';
-
-export default (app: Express) => {
-  const { GQL_API_URL: endpoint } = getEnv();
-  const initPlayground = (expressPlayground as any).default;
-  app.get('/playground', initPlayground({ endpoint }));
-};
