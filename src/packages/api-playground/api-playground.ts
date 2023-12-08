136b2115c7ec3fb9ce6230e7c087f49eb8d99e63
Mythicaeda <theresa.kamerman@jpl.nasa.gov>
2023-12-08 14:48:50 -0800

    Improve altair experience (#50)
    * Automatically set Auth headers and add preRQ script
    
    - Automatically install the GQL explorer plugin
    - Update Altair version
    
    * Update login script to only login when the JWT expires
    
    - Move Login script to external file
    - Exclude `static/` directory from esLint

--- src/packages/api-playground/api-playground.ts
+++ src/packages/api-playground/api-playground.ts
@@ -1,9 +1,34 @@
 import { altairExpress } from 'altair-express-middleware';
 import type { Express } from 'express';
 import { getEnv } from '../../env.js';
+import { readFileSync } from 'fs';
 
 export default (app: Express) => {
   const { GQL_API_URL: endpointURL, GQL_API_WS_URL: subscriptionsEndpoint } = getEnv();
   const initialQuery = '{ plan { id name } }';
-  app.use('/api-playground', altairExpress({ endpointURL, initialQuery, subscriptionsEndpoint }));
+  const initialHeaders = { Authorization: 'Bearer {{user}}', 'x-hasura-role': 'viewer' };
+  const initialPreRequestScript = readFileSync('static/api-playground/pre-request-script.js').toString();
+  const initialSettings = {
+    addQueryDepthLimit: 5,
+    enableExperimental: true,
+    'plugin.list': ['altair-graphql-plugin-graphql-explorer'],
+    'request.withCredentials': true,
+    'schema.reloadOnStart': true,
+    'script.allowedCookies': ['user'],
+    tabSize: 2,
+    theme: 'dracula',
+  };
+
+  app.use(
+    '/api-playground',
+    altairExpress({
+      disableAccount: true,
+      endpointURL,
+      initialHeaders,
+      initialPreRequestScript,
+      initialQuery,
+      initialSettings,
+      subscriptionsEndpoint,
+    }),
+  );
 };
