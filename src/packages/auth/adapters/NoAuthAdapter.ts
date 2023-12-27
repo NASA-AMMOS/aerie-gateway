f03b30100f706c36fc394aae962db5de75f8f6ee
(skovati) Luke <skovati@protonmail.com>
2023-12-27 14:23:44 -0800

    update NoAuthAdapter to use new UI flow

--- src/packages/auth/adapters/NoAuthAdapter.ts
+++ src/packages/auth/adapters/NoAuthAdapter.ts
@@ -1,20 +1,14 @@
 import { getEnv } from '../../../env.js';
-import { generateJwt, getUserRoles } from '../functions.js';
 import type { AuthAdapter, ValidateResponse } from '../types.js';
 
 export const NoAuthAdapter: AuthAdapter = {
   logout: async (): Promise<boolean> => true,
   validate: async (): Promise<ValidateResponse> => {
-    const { DEFAULT_ROLE_NO_AUTH, ALLOWED_ROLES_NO_AUTH } = getEnv();
-
-    console.log('auth disabled, returning default roles');
-    const userId = 'default_user';
-    const { allowed_roles, default_role } = await getUserRoles(userId, DEFAULT_ROLE_NO_AUTH, ALLOWED_ROLES_NO_AUTH);
-
+    const { AUTH_UI_URL } = getEnv();
     return {
-      message: userId,
-      success: true,
-      token: generateJwt(userId, default_role, allowed_roles) ?? undefined,
+      message: 'No auth enabled',
+      redirectURL: AUTH_UI_URL,
+      success: false,
     };
   },
 };
