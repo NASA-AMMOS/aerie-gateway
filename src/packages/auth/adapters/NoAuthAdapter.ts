7b7abc9edba2c175022919ef1dcf54a1e05e0a0d
(skovati) Luke <skovati@protonmail.com>
2024-01-25 12:39:32 -0800

    Throw unsupported configuration error in `NoAuthAdapter`

--- src/packages/auth/adapters/NoAuthAdapter.ts
+++ src/packages/auth/adapters/NoAuthAdapter.ts
@@ -1,14 +1,11 @@
-import { getEnv } from '../../../env.js';
 import type { AuthAdapter, ValidateResponse } from '../types.js';
 
 export const NoAuthAdapter: AuthAdapter = {
   logout: async (): Promise<boolean> => true,
   validate: async (): Promise<ValidateResponse> => {
-    const { AUTH_UI_URL } = getEnv();
-    return {
-      message: 'No auth enabled',
-      redirectURL: AUTH_UI_URL,
-      success: false,
-    };
+    throw new Error(`
+      The UI is configured to use SSO auth, but the Gateway has AUTH_TYPE=none set, which is not a supported configuration.
+      Disable SSO auth on the UI if JWT-only auth is desired.
+    `);
   },
 };
