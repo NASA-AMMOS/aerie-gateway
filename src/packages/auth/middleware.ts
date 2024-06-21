58d2d17379e0ea9142e400c3922b2cf65bc032f4
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-06-21 15:41:31 -0700

    Add auth handler for admin-only endpoints
    - uses the jwt and hasura 'x-hasura-role' headers to authorize
    - validate that the JWT is good (ie unexpired) when `AUTH_TYPE=NONE`

--- src/packages/auth/middleware.ts
+++ src/packages/auth/middleware.ts
@@ -1,20 +1,50 @@
 import type { NextFunction, Request, Response } from 'express';
-import { getEnv } from '../../env.js';
-import { session } from './functions.js';
+import { decodeJwt, session } from './functions.js';
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
-  const { AUTH_TYPE } = getEnv();
+  const authorizationHeader = req.get('authorization');
+  const response = await session(authorizationHeader);
 
-  if (AUTH_TYPE === 'none') {
+  if (response.success) {
     next();
   } else {
-    const authorizationHeader = req.get('authorization');
-    const response = await session(authorizationHeader);
+    res.status(401).send({ message: 'Unauthorized', success: false });
+  }
+};
 
-    if (response.success) {
-      next();
-    } else {
-      res.status(401).send({ message: 'Unauthorized', success: false });
+// Only permits `aerie_admin` users
+export const adminOnlyAuth = async (req: Request, res: Response, next: NextFunction) => {
+  const authorizationHeader = req.get('authorization');
+  const response = await session(authorizationHeader);
+
+  if (response.success) {
+    const { jwtPayload } = decodeJwt(authorizationHeader);
+    if (jwtPayload == null) {
+      res.status(401).send({ message: 'No authorization headers present.' });
+      return;
     }
+
+    const defaultRole = jwtPayload['https://hasura.io/jwt/claims']['x-hasura-default-role'] as string;
+    const allowedRoles = jwtPayload['https://hasura.io/jwt/claims']['x-hasura-allowed-roles'] as string[];
+
+    const { headers } = req;
+    const { 'x-hasura-role': role } = headers;
+
+    if (role != undefined) {
+      if (!allowedRoles.includes(role as string)) {
+        res.status(401).send({ message: 'Declared role is not in allowed roles.' });
+        return;
+      }
+      if (role != 'aerie_admin') {
+        res.status(403).send({ message: 'Current active role is unauthorized.' });
+        return;
+      }
+    } else if (defaultRole != 'aerie_admin') {
+      res.status(403).send({ message: 'Current active role is unauthorized.' });
+      return;
+    }
+    next();
+  } else {
+    res.status(401).send({ message: 'Unauthorized', success: false });
   }
 };
