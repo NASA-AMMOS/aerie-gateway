9c5d054c51782dc54fe48c30899842e917986f25
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-08-20 12:00:06 -0700

    Add /version endpoint

--- src/packages/health/health.ts
+++ src/packages/health/health.ts
@@ -1,4 +1,5 @@
 import type { Express } from 'express';
+import { getEnv } from '../../env.js';
 
 export default (app: Express) => {
   /**
@@ -28,5 +29,31 @@ export default (app: Express) => {
     const timestamp = new Date().toISOString();
     const uptimeMinutes = process.uptime() / 60;
     res.json({ timestamp, uptimeMinutes });
+  });
+
+  /**
+   * @swagger
+   * /version:
+   *   get:
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: Version metrics
+   *         content:
+   *           application/json:
+   *             schema:
+   *                properties:
+   *                  gateway_version:
+   *                    description: The current version of the Aerie Gateway.
+   *                    type: string
+   *     summary: Get the current version of the Gateway and Database Schema
+   *     tags:
+   *       - Version
+   */
+  app.get('/version', async (_, res) => {
+    res.json({
+      version: getEnv().VERSION,
+    });
   });
 };
