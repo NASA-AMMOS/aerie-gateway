3fbecf930880fa4b92ac25dac2e227467c5a2f5e
psubram3 <pranav.subramanian@nasa.gov>
2024-11-18 12:26:23 -0800

    remove console.logs

--- src/packages/external-event/external-event.ts
+++ src/packages/external-event/external-event.ts
@@ -27,12 +27,9 @@ async function uploadExternalEventType(req: Request, res: Response) {
   const headers: HeadersInit = {
     Authorization: authorizationHeader ?? '',
     'Content-Type': 'application/json',
-    'x-hasura-admin-secret': 'aerie', // HACK, TODO: FIX
     'x-hasura-role': roleHeader ? `${roleHeader}` : '',
     'x-hasura-user-id': userHeader ? `${userHeader}` : '',
   };
-
-  console.log("\n\n", JSON.stringify(attribute_schema), "\n\n")
 
   // Validate schema is valid JSON Schema
   try {
