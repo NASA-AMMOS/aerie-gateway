099225ed8edb933cf752861d3dc0cad62be5674a
bduran <bduran@jpl.nasa.gov>
2026-08-05 15:38:49 -0700

    unlikely breaking change

--- static/api-playground/pre-request-script.js
+++ static/api-playground/pre-request-script.js
@@ -1,5 +1,5 @@
 const nowInSeconds = () => Date.now() / 1000;
-const tokenExpiry = await altair.storage.get('token_exp') || 0;
+const tokenExpiry = (await altair.storage.get('token_exp')) || 0;
 
 if (nowInSeconds() >= Number(tokenExpiry)) {
   // Fetch a new token from the Gateway
@@ -7,17 +7,20 @@ if (nowInSeconds() >= Number(tokenExpiry)) {
     'POST',
     '/auth/login', // AUTH ENDPOINT OF THE DEPLOYMENT
     {
-      body: { username: '<YOUR_AERIE_USERNAME>', password: '<YOUR_AERIE_PASSWORD>' }, // CREDENTIALS TO LOG IN AS
-      headers: { 'Content-Type': 'application/json' }
-    });
-  if(res.success) {
+      body: { username: '<YOUR_PLANDEV_USERNAME>', password: '<YOUR_PLANDEV_PASSWORD>' }, // CREDENTIALS TO LOG IN AS
+      headers: { 'Content-Type': 'application/json' },
+    },
+  );
+  if (res.success) {
     const token = res.token;
     await altair.storage.set('token', token);
     // Set JWT expiry
     const atob = await altair.importModule('atob');
     const body = JSON.parse(atob(token.split('.')[1]));
     await altair.storage.set('token_exp', body.exp);
-  } else { altair.log(res); }
+  } else {
+    altair.log(res);
+  }
 }
 // Set the token in the environment
 const token = await altair.storage.get('token');
